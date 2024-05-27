6062d306d3f785616bf8bb0ff7608bfa7b40d6f2
iribastrillo <iribastrillo@gmail.com>
2024-05-27 17:50:34 -0300

    feat: implement add course to career path. Refactor template structure files.

--- src/django/carreras/urls.py
+++ src/django/carreras/urls.py
@@ -3,8 +3,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('crear-curso/', views.createCourse.as_view(), name='create-course'),
-    path('cursos/', views.listCourses.as_view(), name='courses'),
-    path('crear-carrera/', views.createCareer.as_view(), name='create-career'),
-    path('carreras/', views.listCareer.as_view(), name='careers'),
+    path('<pk>/agregar-curso', views.AddCourseToCareer.as_view(), name='add-course'),
+    path('crear-curso/', views.CreateCourse.as_view(), name='create-course'),
+    path('cursos/', views.ListCourses.as_view(), name='courses'),
+    path('crear-carrera/', views.CreateCareer.as_view(), name='create-career'),
+    path('', views.ListCareer.as_view(), name='careers'),  
 ]
--- src/django/carreras/views.py
+++ src/django/carreras/views.py
@@ -1,19 +1,24 @@
-from django.views.generic.edit import CreateView 
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from domain.models import Curso, Carrera
 
 # Create your views here.
-class createCourse (LoginRequiredMixin, CreateView):
+class CreateCourse (LoginRequiredMixin, CreateView):
     model = Curso
     fields = ['nombre', 'descripcion', 'duracion_meses', 'costo', 'fecha_inicio', 'fecha_fin']
     
-class listCourses (LoginRequiredMixin, ListView):
+class ListCourses (LoginRequiredMixin, ListView):
     model = Curso
     
-class createCareer (LoginRequiredMixin, CreateView):
+class CreateCareer (LoginRequiredMixin, CreateView):
     model = Carrera
     fields = ['nombre', 'descripcion', 'duracion_meses', 'costo', 'fecha_baja', 'fecha_alta', 'cursos']
     
-class listCareer (LoginRequiredMixin, ListView):
+class ListCareer (LoginRequiredMixin, ListView):
     model = Carrera
+
+class AddCourseToCareer (LoginRequiredMixin, UpdateView):
+    model = Carrera
+    fields = ['cursos']
+    template_name_suffix = '_add_course'
