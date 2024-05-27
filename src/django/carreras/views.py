28ebecbf310f2493f4493b83366325885b56a3c2
iribastrillo <iribastrillo@gmail.com>
2024-05-27 18:13:48 -0300

    feat.: complete course and career CRUD.

--- src/django/carreras/views.py
+++ src/django/carreras/views.py
@@ -1,9 +1,12 @@
-from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic.list import ListView 
+from django.views.generic import (CreateView,
+                                  ListView,
+                                  DetailView,
+                                  UpdateView,
+                                  DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from domain.models import Curso, Carrera
 
-# Create your views here.
+
 class CreateCourse (LoginRequiredMixin, CreateView):
     model = Curso
     fields = ['nombre', 'descripcion', 'duracion_meses', 'costo', 'fecha_inicio', 'fecha_fin']
@@ -11,12 +14,32 @@ class CreateCourse (LoginRequiredMixin, CreateView):
 class ListCourses (LoginRequiredMixin, ListView):
     model = Curso
     
+class DetailCourse(LoginRequiredMixin, DetailView):
+    model = Curso
+    
+class DeleteCourse(LoginRequiredMixin, DeleteView):
+    model = Curso
+    
+class UpdateCourse(LoginRequiredMixin, UpdateView):
+    model=Curso
+    fields = ['nombre', 'descripcion', 'duracion_meses', 'costo', 'fecha_inicio', 'fecha_fin']
+    
 class CreateCareer (LoginRequiredMixin, CreateView):
     model = Carrera
     fields = ['nombre', 'descripcion', 'duracion_meses', 'costo', 'fecha_baja', 'fecha_alta', 'cursos']
     
 class ListCareer (LoginRequiredMixin, ListView):
     model = Carrera
+    
+class DetailCareer(LoginRequiredMixin, DetailView):
+    model = Carrera
+    
+class DeleteCareer(LoginRequiredMixin, DeleteView):
+    model = Carrera
+    
+class UpdateCareer(LoginRequiredMixin, UpdateView):
+    model = Carrera
+    fields = ['nombre', 'descripcion', 'duracion_meses', 'costo', 'fecha_baja', 'fecha_alta', 'cursos']
 
 class AddCourseToCareer (LoginRequiredMixin, UpdateView):
     model = Carrera
