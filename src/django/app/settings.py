06f009fde8482089f5c14fb045ba1d867ca8524b
Agustin Miller <agusmill@gmail.com>
2024-07-08 20:19:20 -0300

    Se comento linea de app/settings get_role_processor

--- src/django/app/settings.py
+++ src/django/app/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "app.context_processors.get_role_processor",
+                # "app.context_processors.get_role_processor",
             ],
         },
     },
