a09fc41d6298f095d90a1567ce3a8097d57992f8
iribastrillo <iribastrillo@gmail.com>
2024-07-23 17:19:45 -0300

    Update ABM-Examenes

--- src/django/app/context_processors.py
+++
@@ -1,9 +0,0 @@
-def get_role_processor(request):
-    if request.user.is_anonymous:
-        return {}
-    professor = request.user.profesor_set.all()
-    student = request.user.alumno_set.all()
-    if professor.exists():
-        return {"role": professor.first()}
-    if student.exists():
-        return {"role": student.first()}
