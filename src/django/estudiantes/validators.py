4cd9dacfc6172b5c5a11982c2e7b0cfae9a99ef8
iribastrillo <iribastrillo@gmail.com>
2024-07-23 17:30:44 -0300

    REvert ABM examenes

--- src/django/estudiantes/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-
-
-def group_is_not_full(value):
-    if value.alumnos.count() >= value.cupo:
-        raise ValidationError(
-            _("El cupo está lleno."),
-        )
