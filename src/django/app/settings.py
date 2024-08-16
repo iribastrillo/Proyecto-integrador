e8d4d99fa57633ce2301d546a2af3a1da7e240c4
iribastrillo <iribastrillo@gmail.com>
2024-08-15 22:06:08 -0300

    Update files for deploy on Heroku

--- src/django/app/settings.py
+++ src/django/app/settings.py
@@ -150,17 +150,15 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
-
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
 
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 
